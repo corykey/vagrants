bbaa889c099209f466b7871ce9f894a48b11f146
corykey <dev@corykey.com>
2016-09-18 19:47:39 +0200

    install nodeJS with apt-get

--- configs/installs/debian/install_vue.sh
+++ configs/installs/debian/install_vue.sh
@@ -4,18 +4,24 @@
 # dev@corykey.com
 # 2016
 
-NODE_VERSION=v4.x # v0.10.40, v2.0.2, v3.x, v4.x, v5.x, v6.x
+NODE_VERSION=4.x # v0.10.40, v2.0.2, v3.x, v4.x, v5.x, v6.x
 
-apt-get update -y && apt-get upgrade \
-    && apt-get -y install g++ curl libssl-dev \
-    && apt-get install git-core && cd /usr/src \
-    && git clone https://github.com/corykey/node \
-    && cd node && git checkout $NODE_VERSION \
-    && ./configure \
-    && make && make install
+curl -sL https://deb.nodesource.com/setup_${NODE_VERSION} | sudo -E bash - \
+    && apt-get install -y nodejs \
+    && npm install request
+
+    # To compile NodeJS
+    # && apt-get -y install g++ curl libssl-dev \
+    # && apt-get install git-core && cd /usr/src \
+    # && git clone https://github.com/corykey/node \
+    # && cd node && git checkout $NODE_VERSION \
+    # && ./configure \
+    # && make && make install
 
 npm install vue && npm install vue@csp \
-    && npm install -g vue-cli
+    && npm install -g vue-cli \
+    && npm install vue-router \
+    && npm install vue-resource
 
 # curl -sL http://nsolid-deb.nodesource.com/nsolid_setup_1.x | bash - \
 #    && apt-get -y install nsolid-developer-bundle
